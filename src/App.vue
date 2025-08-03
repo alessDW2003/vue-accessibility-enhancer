9d8097cd10fe7a0cb46464397c6db8e3103159f2
Alessandro De Waele <alessandro.dewaele@student.hogent.be>
2025-08-03 16:10:11 +0200

    accesible modal, first button or item is selected, tabnavigation and arrow navigation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <AccessibleTest />
+  <AccesibleModel />
 </template>
 
 <script setup>
 import AccessibleTest from "./components/AccessibleTest.vue"; // pad aanpassen
+import AccesibleModel from "./components/AccesibleModel.vue"; // pad aanpassen
 </script>
---
+++ src/components/AccesibleModel.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from "vue";
+
+const isOpen = ref(false);
+function openModal() {
+  isOpen.value = true;
+}
+function closeModal() {
+  isOpen.value = false;
+}
+
+function test() {
+  alert("Test button clicked!");
+}
+</script>
+
+<template>
+  <div>
+    <button @click="openModal">Open modal</button>
+
+    <div
+      v-if="isOpen"
+      v-accessible-modal
+      class="modal"
+      @modal-close="closeModal"
+      tabindex="-1"
+      role="dialog"
+      aria-modal="true"
+      aria-labelledby="modal-title"
+    >
+      <h2 id="modal-title">Modal titel</h2>
+      <p>Inhoud van de modal</p>
+      <button @click="closeModal">Sluiten</button>
+      <button @click="test">Test</button>
+      <button @click="test">Test2</button>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal {
+  position: fixed;
+  background: white;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  top: 20%;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 1000;
+}
+</style>
