67aadefe34e5ec2dc4bb11e5904dcc90681391ea
Alessandro De Waele <alessandro.dewaele@student.hogent.be>
2025-08-08 09:51:35 +0200

    changes accessibleModal and accessible.js [feature] new accessible component dropdown

--- src/components/AccessibleTest.vue
+++ src/components/AccessibleTest.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    <button v-accessible @click="handleClick">Klik of Enter/Spatie toets</button>
+  <div class="button-container">
+    <div v-accessible="isPressed" @toggle="isPressed = $event.detail">
+      Toggle knop (druk hier of toets Enter/Space)
+    </div>
+
+    <div v-accessible @click="showAlert('Gewone knop geklikt!')">
+      Gewone knop (druk hier of toets Enter/Space)
+    </div>
+
+    <div v-accessible @click="showAlert('Nog een knop!')">
+      Nog een knop (druk hier of toets Enter/Space)
+    </div>
   </div>
 </template>
 
 <script setup>
-function handleClick() {
-  alert('Element geactiveerd!');
+import { ref } from "vue";
+
+const isPressed = ref(false);
+
+function showAlert(message) {
+  alert(message);
+  console.log(message);
 }
 </script>
